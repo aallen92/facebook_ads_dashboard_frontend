db9366f42ef084eddca0d40266094d2d8f71a4b5
Jeremy Thomas <tommyhilfiger12@hotmail.com>
2023-01-14 14:35:05 -0600

    set up firebase to use facebook auth

--- src/Components/FacebookAuth.js
+++ src/Components/FacebookAuth.js
@@ -1,7 +1,8 @@
-import FacebookLogin from "@greatsumini/react-facebook-login";
 import { FaFacebook } from "react-icons/fa";
 import { useBetween } from "use-between";
 import { useShareableState } from "../Context/Session";
+import { signInWithPopup, FacebookAuthProvider } from "firebase/auth";
+import { firebaseAuth } from "../firebase.js";
 
 const useSharedState = () => useBetween(useShareableState);
 
@@ -9,17 +10,29 @@ export const FacebookAuth = () => {
 	const { isLoggedIn, setIsLoggedIn, data, setData, picture, setPicture } =
 		useSharedState();
 
-	const appId = "1372202453543125";
-
-	const responseFacebook = (response) => {
-		console.log(response);
-		if (response.status === "unknown") {
-			alert("Login failed!");
-			setIsLoggedIn(false);
-			return false;
-		}
-		setData(response);
-		setPicture(response.picture.data.url);
+	const facebookLogin = () => {
+		const provider = new FacebookAuthProvider();
+		signInWithPopup(firebaseAuth, provider) // firebaseAuth is from firebase.js
+			.then((result) => {
+				const user = result.user;
+				const credential = FacebookAuthProvider.credentialFromResult(result);
+				const accessToken = credential.accessToken;
+				if (user) {
+					setIsLoggedIn(true);
+					setData(user);
+					setPicture(`${user.photoURL}?access_token=${accessToken}`);
+				}
+			})
+			.catch((error) => {
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				const email = error.email;
+				const credential = FacebookAuthProvider.credentialFromError(error);
+				console.log("errorCode", errorCode);
+				console.log("errorMessage", errorMessage);
+				console.log("email", email);
+				console.log("credential", credential);
+			});
 	};
 
 	const logout = () => {
@@ -28,36 +41,16 @@ export const FacebookAuth = () => {
 		setPicture("");
 	};
 
-	const loginSuccess = (response) => {
-		console.log("Login Success!", response);
-		if (response.accessToken) {
-			setIsLoggedIn(true);
-		} else {
-			setIsLoggedIn(false);
-		}
-	};
-
 	return (
 		<div className="flex justify-center items-center h-[100vh]">
-			{!isLoggedIn && (
-				<FacebookLogin
-					onSuccess={loginSuccess}
-					onFail={(error) => {
-						console.log("Login Failed!", error);
-					}}
-					onProfileSuccess={(response) => {
-						console.log("Get Profile Success!");
-						responseFacebook(response);
-					}}
-					appId={appId}
-					children={
-						<div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-between items-center ">
-							<FaFacebook /> <span className="px-2">Login with Facebook</span>
-						</div>
-					}
-				/>
-			)}
+			<div
+				onClick={facebookLogin}
+				className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-between items-center "
+			>
+				<FaFacebook /> <span className="px-2">Login with Facebook</span>
+			</div>
 
+			{/* currently not being used */}
 			{isLoggedIn && (
 				<div className="card ">
 					<div className="card-body flex justify-between items-center w-full">
